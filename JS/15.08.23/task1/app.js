ccb993a0db978609e916eaedab40b7eab8c7d843
QulamSamadli <qulam999@gmail.com>
2023-08-15 08:45:06 +0400

    task1 added codes

---
+++ JS/15.08.23/task1/app.js
@@ -0,0 +1,4 @@
+let name = prompt("Adinizi qeyd edin")
+let surName = prompt("Soyadinizi qeyd edin")
+let age = +prompt("Yasinizi qeyd edin")
+console.log(` I am ${name}. My surname is ${surName}. I am ${age} years old.`);
