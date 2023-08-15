a5078dab4b1016274ccb22a1d73586ffbd806b15
QulamSamadli <qulam999@gmail.com>
2023-08-15 08:57:16 +0400

    task3 added codes

---
+++ JS/15.08.23/task3/app.js
@@ -0,0 +1,6 @@
+let num1 = +prompt("1-ci ededi daxil edin")
+let num2 = +prompt("2-ci ededi daxil edin")
+console.log(`${num1} + ${num2} = ${num1 + num2}`);
+console.log(`${num1} - ${num2} = ${num1 - num2}`);
+console.log(`${num1} * ${num2} = ${num1 * num2}`);
+console.log(`${num1} / ${num2} = ${num1 / num2}`);
