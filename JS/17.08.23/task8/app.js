8a086dfc9849fed89bea7fd207b91318bf92f399
QulamSamadli <qulam999@gmail.com>
2023-08-17 00:44:40 +0400

    task8 completed

---
+++ JS/17.08.23/task8/app.js
@@ -0,0 +1,10 @@
+let ed = +prompt(" reqemi daxil edin");
+if(ed===0){
+    console.log("ne tekdir ne de cut");
+}
+else if(ed%2===0){
+    console.log("cutdur");
+}
+else if( ed%2!==0){
+    console.log("tekdir");
+}
