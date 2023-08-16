e3b0ed652f82e1b47ece69a5bb4df9a01db97c54
QulamSamadli <qulam999@gmail.com>
2023-08-17 00:56:06 +0400

    task10 completed

---
+++ JS/17.08.23/task10/app.js
@@ -0,0 +1,27 @@
+let ed1 = +prompt("eded daxil edin");
+let ed2 = +prompt("eded daxil edin");
+let operator = prompt("operator daxil edin(+ ve ya -)");
+
+// if(operator === "+"){
+//     console.log(ed1+ed2);
+// }
+// else if(operator === "-"){
+//     console.log(ed1-ed2);
+// }
+// else{
+//     console.log("bele operator qeyd edilmeyib");
+// }
+
+
+
+switch (operator) {
+  case "+":
+    console.log(ed1 + ed2);
+    break;
+  case "-":
+    console.log(ed1 - ed2);
+    break;
+  default:
+    console.log("bele operator qeyd edilmeyib");
+    break;
+}
