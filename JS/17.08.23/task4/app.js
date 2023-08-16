7048236bef7d25bc456188b989a05e3704b6cced
QulamSamadli <qulam999@gmail.com>
2023-08-16 23:56:49 +0400

    Task4 completed

---
+++ JS/17.08.23/task4/app.js
@@ -0,0 +1,35 @@
+let secdiyimMusiqi = prompt(`Sevdiyin musiqinin adini(reqemle bildir) yaz:
+1.wavin' flag
+2.temptation arash
+3.broken angel
+4.aynadan gozel(kio kio)
+`)
+
+if(secdiyimMusiqi==="1"){
+    console.log(`Give me freedom, give me fire
+    Give me reason, take me higher
+    See the champions take the field now
+    You define us, make us feel proud`);
+}
+else if(secdiyimMusiqi==="2"){
+    console.log(`Temptation in my heart
+    I'm burning, I fall apart
+    When the night falls
+    My heart calls`);
+}
+else if(secdiyimMusiqi==="3"){
+    console.log(`I’m so lonely, broken angel
+    I’m so lonely, listen to my heart
+    One and only, broken angel
+    Come and save me before I fall apart`);
+}
+else if(secdiyimMusiqi==="4"){
+    console.log(`Atan seni vermedi
+    aldatdilar vermedi
+    axtarir gozum seni
+    goster uzunu aynadan
+    aynadan aynadan gozel`);
+}
+else{
+    console.log(`sah sah dam dam ar ar Sahdamar Sahdamar Ajdar`);
+}
