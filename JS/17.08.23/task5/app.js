36f6e55eed351fb35fcb1b33b42704c981c11845
QulamSamadli <qulam999@gmail.com>
2023-08-17 00:31:33 +0400

    Math.max and if else task5

--- JS/17.08.23/task5/app.js
+++ JS/17.08.23/task5/app.js
@@ -2,21 +2,25 @@ let ed1=+prompt("1ci ededi daxil edin")
 let ed2=+prompt("2ci ededi daxil edin")
 let ed3=+prompt("3ci ededi daxil edin")
 
-if(         
-    (ed1>ed2 && ed1>ed3)
+// if(         
+//     (ed1>ed2 && ed1>ed3)
    
-){
-    console.log(`en boyuk reqem:${ed1}`);
-}
-else if(         
-    (ed2>ed1 && ed2>ed3)
+// ){
+//     console.log(`en boyuk reqem:${ed1}`);
+// }
+// else if(         
+//     (ed2>ed1 && ed2>ed3)
    
-){
-    console.log(`en boyuk reqem:${ed2}`);
-}
+// ){
+//     console.log(`en boyuk reqem:${ed2}`);
+// }
 
-else{
-    console.log(`en boyuk reqem:${ed3}`);
-}
+// else{
+//     console.log(`en boyuk reqem:${ed3}`);
+// }
+
+console.log(Math.max(ed1,ed2,ed3));
+
+
 
 
