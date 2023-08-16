d379b18fbe213e13c8edea64720d17839084ac39
QulamSamadli <qulam999@gmail.com>
2023-08-17 00:39:30 +0400

    task7 completed

---
+++ JS/17.08.23/task7/app.js
@@ -0,0 +1,11 @@
+let ed = +prompt("Reqem daxil ediniz");
+
+if(ed>0){
+    console.log(`${ed} musbetdir`);
+}
+else if(ed<0){
+    console.log(`${ed} menfidir`);
+}
+else{
+    console.log(`${ed}: 0di`);
+}
