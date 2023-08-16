5a07b3ed38a6ade465bf63c0c9210561ba5a5787
QulamSamadli <qulam999@gmail.com>
2023-08-17 01:16:17 +0400

    task15 completed

---
+++ JS/17.08.23/task15/app.js
@@ -0,0 +1,11 @@
+let ed1 = +prompt("eded daxil edin");
+let ed2 = +prompt("eded daxil edin");
+let ed3 = +prompt("eded daxil edin");
+
+let cem = ed1+ed2+ed3
+if(cem>100){
+    console.log(` cemleri edir${cem} ve 100u de kecdi`);
+}
+else{
+    console.log(`cemi edir ${cem} ve 100u kecmedi`);
+}
