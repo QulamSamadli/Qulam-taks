ee725022933b652ac67ea931eea19d4986d64a6c
QulamSamadli <qulam999@gmail.com>
2023-08-19 08:48:16 +0400

    task1

---
+++ JS/19.08.23/task1/app.js
@@ -0,0 +1,16 @@
+let text = prompt(`Istiqametleri ingilisce yazin!`);
+if(text===`up`){
+    console.log(`yuxari get`);
+}
+else if(text===`right`){
+    console.log(`saga get`);
+}
+else if(text===`left`){
+    console.log(`sola get`);
+}
+else if(text===`down`){
+    console.log(`asagi get`);
+}
+else{
+    console.log(`oz oxu etrafinde doner`);
+}
