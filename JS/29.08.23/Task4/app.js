76376365e05a571542dbf399f2f8b7d6f00726d4
QulamSamadli <qulam999@gmail.com>
2023-08-27 22:37:04 +0400

    Task4 completed

---
+++ JS/29.08.23/Task4/app.js
@@ -0,0 +1,12 @@
+// 2. İstifadəçi adını parametr kimi qəbul edən və konsola «[İstifadəçi adı] is active now» mesajı çıxaran funksiya tərtib edin.
+// Funksiyanı 3 müxtəlif ad üçün çağırın.
+
+
+const logGreeting = (ad) => {
+    console.log(`${ad} is active now`);
+  };
+  
+  logGreeting(`Qulam`);
+  logGreeting(`Qulam1`);
+  logGreeting(`Qulam2`);
+  
