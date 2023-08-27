dc6bdae97bb8ebbb3b03a1075798de3a80ae6ce2
QulamSamadli <qulam999@gmail.com>
2023-08-27 23:49:40 +0400

    Task9 completed

---
+++ JS/29.08.23/Task9/app.js
@@ -0,0 +1,11 @@
+// Üç ədəddən ən böyüyünü təyin edən funksiya tərtib edin.
+// Parametr olaraq üç ədəd qəbul edən və konsolda ən böyüyünü göstərən bir funksiya tərtib edin.
+// Fərqli arqumentlərlə funksiyanı üç dəfə çağırın
+
+
+const SHOW_MAX_NUMBER=(a,b,c)=>{
+console.log(Math.max(a,b,c));
+}
+SHOW_MAX_NUMBER(4,5,6)
+SHOW_MAX_NUMBER(9,6,5)
+SHOW_MAX_NUMBER(6,10,4)
