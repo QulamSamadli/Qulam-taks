a7b412405a2acd14ba821f298b25142645e2e491
QulamSamadli <qulam999@gmail.com>
2023-08-27 22:33:59 +0400

    Task3 completed

---
+++ JS/29.08.23/Task3/app.js
@@ -0,0 +1,7 @@
+// 1. logGreeting() adında funksiya tərtib edin hansıki konsola «[Adınız] is a JS developer» mesajı çıxarsın.
+
+const logGreeting = (ad) => {
+  console.log(`${ad} is a JS developer`);
+};
+
+logGreeting(`Qulam`);
