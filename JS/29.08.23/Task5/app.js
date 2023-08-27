f9ca997df5fb2e8137ee0d3e76bb179e7e9cd449
QulamSamadli <qulam999@gmail.com>
2023-08-27 22:39:16 +0400

    Task5 completed

---
+++ JS/29.08.23/Task5/app.js
@@ -0,0 +1,8 @@
+// 3. Parametr kimi ədədi qəbul edən və onu dördüncü dərəcə qüvvətə yülsəldərək konsolda göstərən fourth() funksiyasını yazın.
+
+
+
+const numPower = (eded)=>{
+ console.log(Math.pow(eded,4));
+}
+numPower(2)
