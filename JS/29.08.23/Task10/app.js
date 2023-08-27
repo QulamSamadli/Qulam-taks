a3aaea791657e03b8dabe79256817f38a9b5dd74
QulamSamadli <qulam999@gmail.com>
2023-08-27 23:54:47 +0400

    Task10 completed

---
+++ JS/29.08.23/Task10/app.js
@@ -0,0 +1,14 @@
+// Task-03
+// Bütün hesabların cəmini rubl ilə mesaj çıxaran funksiya tərtib edin.
+// Rubl və dollarla hesab qəbul edən və bütün hesablardakı məbləğin
+// cəmin rublla göstərən funksiya yazın.
+// Tutaq ki, müştərinin rubl hesabında 1200 rubl, dollar hesabında isə 20 dollar var.
+// Bir dollar üçün məzənnə 75 rubl təşkil edir.
+// Bu halda, funksiya belə mesaj göstərməlidir: Bütün depozitlər üzrə məbləğ: 2700 rubl.
+
+const TO_CHANGE_RUBL = (dollar, rubl) => {
+  let dollarToRubl = dollar * 75;
+  console.log(dollarToRubl + rubl);
+};
+
+TO_CHANGE_RUBL(20, 1200);
