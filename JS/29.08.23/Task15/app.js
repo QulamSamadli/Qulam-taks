b91a45db8248da608291b853312962046569e89a
QulamSamadli <qulam999@gmail.com>
2023-08-28 14:32:48 +0400

    Task15 completed

---
+++ JS/29.08.23/Task15/app.js
@@ -0,0 +1,12 @@
+// Bir ədədi parametr kimi qəbul edən və cüt olduqda true, 
+// tək olduqda isə false qaytaran isEven () funksiyasını yazın.
+
+const isEven = (eded)=>{
+
+    if(eded%2===0){
+console.log(`${eded} is even`);
+    }
+else(console.log(`${eded} tekdir`));
+
+}
+isEven(17)
