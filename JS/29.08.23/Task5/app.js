da0bbbe6dd1b2f72615e87a542d3a55c641f5304
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-08-29 10:55:59 +0400

    live legend 🐱‍💻

--- JS/29.08.23/Task5/app.js
+++ JS/29.08.23/Task5/app.js
@@ -1,7 +1,7 @@
 // 3. Parametr kimi ədədi qəbul edən və onu dördüncü dərəcə qüvvətə yülsəldərək konsolda göstərən fourth() funksiyasını yazın.
 
 
-
+// A: live legend 🐱‍💻
 const numPower = (eded)=>{
  console.log(Math.pow(eded,4));
 }
