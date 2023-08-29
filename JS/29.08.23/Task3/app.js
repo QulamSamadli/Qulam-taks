f7f2f4293bf6c28111777a7190e521c610bd7b07
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-08-29 10:52:18 +0400

    u r best, milord 👑

--- JS/29.08.23/Task3/app.js
+++ JS/29.08.23/Task3/app.js
@@ -1,5 +1,5 @@
 // 1. logGreeting() adında funksiya tərtib edin hansıki konsola «[Adınız] is a JS developer» mesajı çıxarsın.
-
+// A: u r best, milord 👑
 const logGreeting = (ad) => {
   console.log(`${ad} is a JS developer`);
 };
