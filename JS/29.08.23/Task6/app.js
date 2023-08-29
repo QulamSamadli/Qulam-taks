d94baec0e2ad6bc604227ad6fa2b4e9adfdeaf0d
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-08-29 10:52:23 +0400

    u r best, milord 👑

--- JS/29.08.23/Task6/app.js
+++ JS/29.08.23/Task6/app.js
@@ -1,5 +1,6 @@
 // 4. Sonu 0 ilə bitən bütün üçrəqəmli ədədləri konsola çıxaran proqram yazın. (for ile yazin)
 
+// A: u r best, milord 👑
 for(let i = 100; i<1000; i+=10){
     console.log(i);
   }
