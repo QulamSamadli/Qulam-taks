fd8bcb7d1f4c958b983c6f3905f38e9b5bff4841
QulamSamadli <qulam999@gmail.com>
2023-09-05 10:17:21 +0400

    Task2 completed

---
+++ JS/05.09.23/Task2/app.js
@@ -0,0 +1,5 @@
+
+let snakeToKebab = `SNAKE_CASE`
+console.log(snakeToKebab.toUpperCase().replace(`to`,`_`));
+let kebabCase= `BU_TEST_UCUNDUR`
+console.log(kebabCase.toLowerCase().replaceAll(`_`,`-`));
