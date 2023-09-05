98e2a4f5a32f7a5459bcfb1c5bf2cdefb2907fea
QulamSamadli <qulam999@gmail.com>
2023-09-05 11:39:56 +0400

    Task13 completed

---
+++ JS/05.09.23/Task13/app.js
@@ -0,0 +1,9 @@
+function isvalidnumber(tel) {
+  if (tel.startsWith("+7") && tel.length === 11) {
+    console.log(`nomre duzgun qyed edilmisdir`);
+  } else {
+    console.log(`nomre duzgun qeyd edilmemisdir`);
+  }
+}
+
+isvalidnumber(`+7523456789`);
