30df8c6783d3ae9a3191949eb03eb71f828c1e99
QulamSamadli <qulam999@gmail.com>
2023-09-05 10:58:55 +0400

    Task6 completed

---
+++ JS/05.09.23/Task6/app.js
@@ -0,0 +1,10 @@
+
+let gmail = prompt(`mail adressinizi qeyd edin`)
+
+
+if(gmail.length>14 && gmail.endsWith(`@gmail.com`)){
+    console.log(gmail);
+}
+else{
+    console.log(`Yanlis mail adreessi daxil etmisiniz`);
+}
