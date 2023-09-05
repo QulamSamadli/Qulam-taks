2e63a37c354b1342e34cb5a41966aa4f2648f777
QulamSamadli <qulam999@gmail.com>
2023-09-05 11:22:52 +0400

    Task10 completed

---
+++ JS/05.09.23/Task10/app.js
@@ -0,0 +1,10 @@
+let eded = prompt(`Eded daxil edin`);
+
+let sol = Number(eded[0]) + Number(eded[1]) + Number(eded[2]);
+let sag = Number(eded[3]) + Number(eded[4]) + Number(eded[5]);
+
+if (sol === sag) {
+  console.log(`Bilet sanslidir`);
+} else {
+  console.log(`Bextinizi bir de sinayin`);
+}
