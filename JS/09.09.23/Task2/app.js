9bea0a508689c20e33615398f06139ea12bcb7d1
QulamSamadli <qulam999@gmail.com>
2023-09-09 13:40:36 +0400

    Task2 completed

---
+++ JS/09.09.23/Task2/app.js
@@ -0,0 +1,16 @@
+let arr = +prompt(`daxil edin`)
+let arr1 = +prompt(`daxil edin`)
+let arr2 = +prompt(`daxil edin`)
+let arr3 = +prompt(`daxil edin`)
+let arr4 = +prompt(`daxil edin`)
+let b = []
+
+b.push(arr)
+b.push(arr1)
+b.push(arr2)
+b.push(arr3)
+b.push(arr4)
+
+b.length=5
+
+console.log(b);
