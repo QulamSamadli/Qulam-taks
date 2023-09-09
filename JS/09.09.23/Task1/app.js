558ce625f9d40a63668b4a9fe379c7b93a386f05
QulamSamadli <qulam999@gmail.com>
2023-09-09 17:12:33 +0400

    changes code

--- JS/09.09.23/Task1/app.js
+++ JS/09.09.23/Task1/app.js
@@ -1,8 +1,8 @@
-const multiple =(arr)=>{
+const multiple =(...arr)=>{
 let m = 1
 for(let values of arr){
   m=m*values
 }
 return m
 }
-console.log(multiple([5,6,3]));
+console.log(multiple(5,6,3));
