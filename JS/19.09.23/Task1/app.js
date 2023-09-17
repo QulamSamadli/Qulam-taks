83983bc80f2aedf89d1c5911151a11bca9424026
QulamSamadli <qulam999@gmail.com>
2023-09-18 00:34:36 +0400

    Task1 completed

---
+++ JS/19.09.23/Task1/app.js
@@ -0,0 +1,18 @@
+const first = document.getElementById(`firstCircle`);
+const second = document.getElementById(`secondCircle`);
+const third = document.getElementById(`thirdCircle`);
+const four = document.getElementById(`fourCircle`);
+
+first.onclick = () => {
+
+  document.body.style.backgroundColor = `blue`;
+};
+second.onclick = () => {
+  document.body.style.backgroundColor = `red`;
+};
+third.onclick = () => {
+  document.body.style.backgroundColor = `green`;
+};
+four.onclick = () => {
+  document.body.style.backgroundColor = `yellow`;
+};
